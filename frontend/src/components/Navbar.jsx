8cf078be9205e82d5d3148907fc09587a2684980
Leslie Garcia <ggarciaveka@gmail.com>
2026-07-29 16:39:32 -0500

    Ft navbar: navbar styles and sidebar for responsive

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,18 @@
 import { useState } from "react";
 import { screenSize } from "./hooks/screenSize";
+import Sidebar from "./Sidebar";
 
 export default function Navbar() {
   const { isMobile } = screenSize();
-  const {isOpen, setIsOpen} = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
+    <>
+
     <div className="fixed h-16 top-0 left-0 w-full z-50 bg-background flex items-center px-4 justify-between">
       <h3 className="font-title">PhysaFlow</h3>
       {isMobile ? (
-        <button  onClick={()=>setIsOpen(true)}>menu</button>
+        <button  onClick={()=>{setIsOpen(true),console.log("perro")}}>menu</button>
       ) : (
         <>
           <div className="flex gap-4">
@@ -19,7 +22,14 @@ export default function Navbar() {
           <button className="rounded-full bg-gold-lightest p-2">calcular</button>
         </>
       )}
-
     </div>
+          {isMobile && (
+        <Sidebar
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+        />
+      )}
+          </>
+    
   );
 }
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,14 +1,31 @@
-export default function Sidebar(){
+export default function Sidebar({ isOpen, setIsOpen }){
     return (
-        <div className="bg-green-200">
-            <h3>Physaflow</h3>
-            <div>
-                <div>botones</div>
-                <div>botones</div>
-            </div>
-            <div>calcular</div>                         
+    <aside
+      className={`
+        fixed top-0 right-0 z-50 h-screen w-72
+        bg-background
+        transform transition-transform duration-300
+        ${isOpen ? "translate-x-0" : "translate-x-full"}
+      `}
+    >
+      <div className="flex h-16 items-center justify-between px-4">
+        <h3 className="font-title">PhysaFlow</h3>
 
-            </div>
+        <button onClick={() => setIsOpen(false)}>
+          ✕
+        </button>
+      </div>
+
+      <nav className="flex flex-col gap-6 p-6">
+        <a href="#">Inicio</a>
+        <a href="#">Dashboard</a>
+        <a href="#">Información</a>
+
+        <button className="rounded-full bg-gold-lightest p-2">
+          Calcular
+        </button>
+      </nav>
+    </aside>
     
     )
 }
