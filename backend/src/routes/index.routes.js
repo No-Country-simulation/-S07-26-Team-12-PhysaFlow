0e67e7e82d34066bff1300e3af4ea43ecc02aa00
Tomas Hernandez <tomasgabrielh@gmail.com>
2026-08-05 17:02:25 -0300

    Se agrego index.routes.js con su estructura correspondiente

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -1,4 +1,13 @@
 import {Router} from "express"
+import calculationRoutes from "./calculation.routes.js";
+import leadRoutes from "./lead.rotes.js";
+import sharedResultRoutes from "./sharedResultRoutes.js";
+import sharedResultRoutes from "./sharedResult.routes.js";
 
-const indexRouter = Router()
-export default indexRouter
+const router = Router();
+
+router.use("/calculations", calculationRoutes);
+router.use("/leads", leadRoutes);
+router.use("/shared-results", sharedResultRoutes);
+
+export default router;
