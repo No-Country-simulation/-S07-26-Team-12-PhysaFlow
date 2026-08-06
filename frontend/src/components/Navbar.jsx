cc6f4fa28f0b983bd4ac228e1b11318900c393f7
Leslie Garcia <ggarciaveka@gmail.com>
2026-08-06 11:42:28 -0500

    ft-components: Button created, recieves props

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { screenSize } from "./hooks/screenSize";
 import Sidebar from "./Sidebar";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   const { isMobile } = screenSize();
@@ -10,8 +11,9 @@ export default function Navbar() {
     <>
 
     <div className="fixed h-16 top-0 left-0 w-full z-50 bg-background flex items-center px-4 justify-between">
-      
-      <img src="/physaflow.png" alt="PhysaFlow" className="w-10 h-10" />
+      <Link to="/">
+        <img src="/physaflow.png" alt="PhysaFlow" className="w-10 h-10" />
+      </Link>
       <h3 className="font-title">PhysaFlow</h3>
       {isMobile ? (
         <button onClick={() => setIsOpen(true)}>menu</button>
